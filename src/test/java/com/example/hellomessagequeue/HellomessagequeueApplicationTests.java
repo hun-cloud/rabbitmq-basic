c0752a562cd8b2532e6b7bf4acb4fdd09d3bf514
JaeHunJaeHun <jhl9838@gmail.com>
2025-01-27 17:10:35 +0900

    init

---
+++ src/test/java/com/example/hellomessagequeue/HellomessagequeueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellomessagequeue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellomessagequeueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
